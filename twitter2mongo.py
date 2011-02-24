549e5c5643be48496cd4d264d9fe710a4a681406
Jay Luker <jluker@cfa.harvard.edu>
2011-02-24 15:53:56 -0500

    * swapped order of url path variables * added some overengineered logging

--- twitter2mongo.py
+++ twitter2mongo.py
@@ -4,28 +4,54 @@ import urllib2
 import urllib
 import simplejson
 import time
+from os.path import dirname, abspath
 from twarchive import *
 from optparse import OptionParser
 from pymongo import Connection
 from uuid import uuid4
 
+import logging
+
+def init_logging(logfile, verbose=False, debug=False):
+    logging.basicConfig(
+        filename = logfile, 
+        level = logging.INFO,
+        format = '%(asctime)s %(levelname)s %(message)s'
+    )
+    log = logging.getLogger()
+    if verbose:
+        log.addHandler(logging.StreamHandler(sys.stdout))
+    if debug:
+        log.setLevel(logging.DEBUG)
+        fmt = logging.Formatter('%(asctime)s %(levelname)s %(thread)d %(filename)s %(lineno)d %(message)s')
+        for h in log.handlers:
+            h.setFormatter(fmt)
+    return log
+
 if __name__ == '__main__':
 
     op = OptionParser()
     op.set_usage("usage: twitter2mongo.py [options] query [query, ...] ")
+    op.add_option('--verbose', dest='verbose', action='store_true',
+        help='write log output to stdout', default=False)
     op.add_option('--debug', dest='debug', action='store_true',
         help='include debugging info in log output', default=False)
+    op.add_option('--logfile', dest='logfile', action='store',
+        help='write to this logfile', 
+        default='%s/twitter2mongo.log' % dirname(dirname(abspath(__file__))))
     opts, args = op.parse_args()
+
+    log = init_logging(opts.logfile, verbose=opts.verbose, debug=opts.debug)
 
     # use 1st positional argument as the db name
     db = get_database(args[0])
-    print "Using database %s" % db.name
+    log.debug("Using database %s" % db.name)
     tweet_collection = get_collection(db, 'tweets')
     query_collection = get_collection(db, 'queries')
 
     for query in args:
         try:
-            print "searching for: %s" % query
+            log.info("searching for: %s" % query)
             tweets, query_meta = basic_search(query)
         except:
             raise
@@ -40,5 +66,5 @@ if __name__ == '__main__':
                 tweet['_id'] = tweet_id
                 tweet['query_id'] = query_id
                 tweet_collection.update({'_id': tweet_id}, tweet, upsert=True)
-            print "Saved %d tweets" % len(tweets)
+            log.info("Saved %d tweets" % len(tweets))
 
--- www/www.py
+++ www/www.py
@@ -9,19 +9,20 @@ site.addsitedir(dirname(dirname(abspath(__file__))))
 import web
 from web.webapi import badrequest, header, NotFound
 import pymongo
+from pymongo import ASCENDING,DESCENDING
 from simplejson import dumps
 from twarchive import *
 
 urls = (
-    '/tweets/([\w]+)$', 'Tweets',
-    '/queries/([\w]+)$', 'Queries',
+    '/([\w]+)/tweets/?$', 'Tweets',
+    '/([\w]+)/queries/?$', 'Queries',
 )
 
 class Tweets:
     def GET(self, dbname):
         db = get_database(dbname)
         collection = get_collection(db, 'tweets')
-        tweets = list(collection.find({}))
+        tweets = list(collection.find({}, sort=[('_id', DESCENDING)]))
         header('Content-type', 'application/json; charset=utf-8')
         return dumps(tweets)
 
@@ -29,7 +30,7 @@ class Queries:
     def GET(self, dbname):
         db = get_database(dbname)
         collection = get_collection(db, 'queries')
-        queries = list(collection.find({}))
+        queries = list(collection.find({}, sort=[('timestamp', DESCENDING)]))
         header('Content-type', 'application/json; charset=utf-8')
         return dumps(queries)
 
